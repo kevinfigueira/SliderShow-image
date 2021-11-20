69a102882b776e3110a1f2fd0710774aed2f30b4
Kevin Figueira <kevinfigueira@hotmail.com>
2021-11-20 16:40:17 -0400

    Update

---
+++ script.js
@@ -0,0 +1,36 @@
+//Select DOM
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
+let first = document.querySelector('.first');
+
+let cont = 0;
+
+//Events
+next.addEventListener('click', imgNext);
+prev.addEventListener('click', imgPrev);
+
+//Functions
+function imgNext(){
+    
+    cont += 20;
+
+    first.style.marginLeft = `-${cont}%`;
+
+    if(cont > 80){
+        cont = 0
+        first.style.marginLeft = `-${cont}%`;
+    }
+    console.log(cont)
+}
+function imgPrev(){
+
+    if(cont === 0){
+        cont = 0;
+    }else{
+        cont -= 20;
+        first.style.marginLeft = `-${cont}%`;
+    }
+    
+
+    console.log(cont)
+}
